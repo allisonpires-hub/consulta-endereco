16cd5ecb6a11b47ba2875ec2a6edbd988a31f71c
allisonpires-hub <allisonabreupires@gmail.com>
2026-09-02 15:26:05 -0300

    Add files via upload
    Adicionar aplicação de consulta de endereço

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="consulta-endereco-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./data.json","./manifest.webmanifest"]))).then(()=>self.skipWaiting()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
